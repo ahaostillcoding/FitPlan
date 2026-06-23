043394ed24095a341f0d7deb2eb81491fc36d7f2
14760 <14760@users.noreply.github.com>
2026-06-23 12:13:26 +0800

    Improve Windows Gradle bootstrap

--- gradlew.bat
+++ gradlew.bat
@@ -1,18 +1,16 @@
-@rem
-@rem Gradle startup script for Windows
-@rem
-
 @if "%DEBUG%"=="" @echo off
+setlocal
+
 set APP_HOME=%~dp0
-set APP_NAME=Gradle
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set GRADLE_VERSION=9.4.1
+set GRADLE_CACHE=%USERPROFILE%\.gradle\wrapper\dists\fitplan-gradle-%GRADLE_VERSION%
+set GRADLE_EXE=%GRADLE_CACHE%\gradle-%GRADLE_VERSION%\bin\gradle.bat
 
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
-if %ERRORLEVEL% equ 0 goto execute
+if %ERRORLEVEL% equ 0 goto ensureGradle
 
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
@@ -23,20 +21,33 @@ goto fail
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%\bin\java.exe
 
-if exist "%JAVA_EXE%" goto execute
+if exist "%JAVA_EXE%" goto ensureGradle
 
 echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
 goto fail
 
+:ensureGradle
+if exist "%GRADLE_EXE%" goto execute
+
+echo Downloading Gradle %GRADLE_VERSION%...
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+  "$ErrorActionPreference='Stop';" ^
+  "$ProgressPreference='SilentlyContinue';" ^
+  "$cache='%GRADLE_CACHE%';" ^
+  "$zip=Join-Path $cache 'gradle-%GRADLE_VERSION%-bin.zip';" ^
+  "New-Item -ItemType Directory -Force -Path $cache | Out-Null;" ^
+  "Invoke-WebRequest -Uri 'https://services.gradle.org/distributions/gradle-%GRADLE_VERSION%-bin.zip' -OutFile $zip;" ^
+  "Expand-Archive -LiteralPath $zip -DestinationPath $cache -Force;"
+if %ERRORLEVEL% neq 0 goto fail
+
 :execute
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% -Dorg.gradle.appname="%APP_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+"%GRADLE_EXE%" %*
 if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
 exit /b 1
 
 :mainEnd
-if "%OS%"=="Windows_NT" endlocal
-
+endlocal
